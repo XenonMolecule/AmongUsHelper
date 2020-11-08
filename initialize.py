142b5cda3bea7b6ad1d5ce52e0f6f1a08f666c9e
Luca C <luca.a.cotter@gmail.com>
2020-11-07 22:59:14 -0800

    Update initialize.py

--- initialize.py
+++ initialize.py
@@ -1,16 +1,23 @@
 import sys
 from datetime import datetime
 import pyautogui
+import cv2
+import time
+import numpy as np
 
 start = datetime.now()
 
-while True: 
-    if pyautogui.locateOnScreen('shh.png', grayscale=False, confidence=.5):
+while True:
+    if pyautogui.locateOnScreen('shh_small.png', grayscale=False, confidence=.5):
         break
 
-    elif (datetime.now() - start).seconds > 30:
-        sys.exit()
+    # elif (datetime.now() - start).seconds > 30:
+        # sys.exit()
 
     else:
         print('No shh detected')
 
+time.sleep(4.63)
+screen = np.array(pyautogui.screenshot())
+cv2.imshow("bruh", screen)
+cv2.waitKey(0)
