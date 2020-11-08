92bd08ff2dec289476d3fec72e3d6deaff26034e
Luca C <luca.a.cotter@gmail.com>
2020-11-08 05:18:01 -0800

    Now uses ColorReader to get all the colors and number of crewmates

--- initialize.py
+++ initialize.py
@@ -8,6 +8,7 @@ import numpy as np
 from fuzzywuzzy import fuzz
 from skimage.filters import threshold_otsu
 from skimage.color import rgb2gray
+from color_reader import ColorReader
 
 start = datetime.now()
 
@@ -25,10 +26,14 @@ CREW_POSITIONS = [CREW_ONE, CREW_TWO, CREW_THREE, CREW_FOUR, CREW_FIVE,
                   CREW_SIX, CREW_SEVEN, CREW_EIGHT, CREW_NINE]
 
 # INITIALIZE SETTINGS
-ROOM_TEXT_START_X = 1330
-ROOM_TEXT_START_Y = 1817
-ROOM_TEXT_WIDTH = 685
-ROOM_TEXT_HEIGHT = 140
+# ROOM_TEXT_START_X = 1330
+# ROOM_TEXT_START_Y = 1817
+# ROOM_TEXT_WIDTH = 685
+# ROOM_TEXT_HEIGHT = 140
+ROOM_TEXT_START_X = 1030
+ROOM_TEXT_START_Y = 1320
+ROOM_TEXT_WIDTH = 500
+ROOM_TEXT_HEIGHT = 85
 
 SNIP_REGION = (
     ROOM_TEXT_START_X,
@@ -45,7 +50,7 @@ ROOMS = ["Cafeteria", "Weapons", "O2", "Navigation",
 MIN_STRING_MATCH_RATIO = 60
 
 
-def segment_crew(crew_count):
+def segment_crew():
     for f in os.listdir("./player_imgs/"):
         os.remove(os.path.join("./player_imgs/", f))
 
@@ -54,6 +59,8 @@ def segment_crew(crew_count):
     screen = cv2.resize(screen, dsize=(3360, 2100), interpolation=cv2.INTER_CUBIC)
     # screen = cv2.imread("segmentation_test.png")
 
+    crew_count, colors = ColorReader.color_reader(screen)
+    colors = [color.upper() for color in colors]
     for i in range(crew_count):
         pos = CREW_POSITIONS[i]
         image = screen[pos[1]:pos[1] + pos[3], pos[0]:pos[0] + pos[2]]
@@ -62,16 +69,20 @@ def segment_crew(crew_count):
         height = int(image.shape[0] * scale_percent)
         dim = (width, height)
         resized = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
-        cv2.imwrite("player_imgs/crew" + str(i + 1) + "-1.png", resized)
-        cv2.imwrite("player_imgs/crew" + str(i + 1) + "-2.png", cv2.flip(resized, 1))
+        cv2.imwrite("player_imgs/crew" + str(colors[i]) + "-1.png", resized)
+        cv2.imwrite("player_imgs/crew" + str(colors[i]) + "-2.png", cv2.flip(resized, 1))
 
     # cv2.imshow("bruh", screen)
     # cv2.waitKey(0)
-
+    cv2.imwrite("crewmatemenu.png", screen)
 
 # Returns the best guess for the room that the player is in or None if it can't get a good guess
 def getPlayerRoom():
     im = pyautogui.screenshot(region=SNIP_REGION)
+    im.save('roomname.png')
+
+    background = pyautogui.screenshot()
+    background.save('roomname_background.png')
 
     gray_img = rgb2gray(np.array(im))
 
@@ -90,6 +101,7 @@ def getPlayerRoom():
     text = ""
     try:
         text = pytesseract.image_to_string(image, config=config)
+        print("text")
     except:
         pass
 
@@ -97,6 +109,8 @@ def getPlayerRoom():
     room = None
     for room_name in ROOMS:
         ratio = fuzz.ratio(room_name.lower(), text.lower())
+        print("ratio " + str(ratio))
+        print(best_ratio)
         if ratio > best_ratio:
             best_ratio = ratio
             room = room_name
@@ -106,11 +120,14 @@ def getPlayerRoom():
 def detect_crewmates(room):
     for img_obj in os.scandir("./player_imgs/"):
         if pyautogui.locateOnScreen("./player_imgs/" + img_obj.name, grayscale=False, confidence=.35):
-            print("CREW" + img_obj.name[4] + " - " + room)
+            name = img_obj.name
+            color = name[:-6]
+            color = color[4:]
+            print("CREW " + color + " - " + room)
 
 while True:
     time.sleep(0.1)
-    if pyautogui.locateOnScreen('crewtext-xenon.png', grayscale=False, confidence=.5): # shh_small
+    if pyautogui.locateOnScreen('crewtext.png', grayscale=False, confidence=.5): # shh_small
         break
 
     # elif (datetime.now() - start).seconds > 30:
@@ -119,17 +136,17 @@ while True:
     else:
         print('No shh detected')
 print("Saw shh")
-# time.sleep(1) # 2.66
+time.sleep(1) # 2.66
 
-segment_crew(3)
+segment_crew()
 
 last_room = "Cafeteria"
 while True:
     time.sleep(0.1)
     new_room = getPlayerRoom()
+    print(new_room)
     if new_room:
         if last_room != new_room:
             print("Just entered " + new_room)
         last_room = new_room
     detect_crewmates(last_room)
-
