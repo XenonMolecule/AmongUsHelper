67c1f08b0355f39bc9962efe914c0e7bace1864c
Luca C <luca.a.cotter@gmail.com>
2020-11-07 20:44:35 -0800

    Accounts for variability in crewmate count

--- color_reader.py
+++ color_reader.py
@@ -44,6 +44,7 @@ class ColorReader:
 
         print(width, height)
         used_colors = []
+        num_crewmates = 0
         for pos_num in range(len(poscoords)):
             print("Colors in Position " + str(pos_num) + ":")
             coords = poscoords[pos_num]
@@ -57,18 +58,34 @@ class ColorReader:
             unknown_color = np.array([r, g, b])
             shortest_dist = sys.maxsize
             print(unknown_color)
-            for i in range(len(list_colors)):
-                squared_dist = np.sum((unknown_color-list_colors[i])**2, axis=0)
-                dist = np.sqrt(squared_dist)
-                if  abs(unknown_color[0] - unknown_color[1]) < 20 and abs(unknown_color[0] - unknown_color[2]) < 20 and unknown_color[0] > 90:
-                    shortest_dist = 0
-                    color_text = "white"
-                if  abs(unknown_color[0] - unknown_color[1]) < 5 and abs(unknown_color[0] - unknown_color[2]) > 20:
-                    shortest_dist = 0
-                    color_text = "yellow"
-                elif shortest_dist > dist and list_colortexts[i] not in used_colors:
-                    shortest_dist = dist
-                    color_text = list_colortexts[i]
+            if pos_num == 0 and abs(r - 37) < 5 and abs(b - 68) < 5 and abs(g - 67) < 5:
+                print("no crewmate")
+            elif pos_num == 9 and abs(r - 27) < 5 and abs(b - 47) < 5 and abs(g - 46) < 5:
+                print("no crewmate")
+            elif pos_num == 8 and abs(r - 39) < 5 and abs(b - 71) < 5 and abs(g - 70) < 5:
+                print("no crewmate")
+            elif pos_num == 1 and abs(r - 53) < 5 and abs(b - 95) < 5 and abs(g - 93) < 5:
+                print("no crewmate")
+            elif pos_num == 7 and abs(r - 54) < 5 and abs(b - 97) < 5 and abs(g - 96) < 5:
+                print("no crewmate")
+            elif pos_num == 2 and abs(r - 75) < 5 and abs(b - 133) < 5 and abs(g - 134) < 5:
+                print("no crewmate")
+            else:
+                for i in range(len(list_colors)):
+                    squared_dist = np.sum((unknown_color-list_colors[i])**2, axis=0)
+                    dist = np.sqrt(squared_dist)
+                    if  abs(unknown_color[0] - unknown_color[1]) < 20 and abs(unknown_color[0] - unknown_color[2]) < 20 and unknown_color[0] > 90:
+                        shortest_dist = 0
+                        color_text = "white"
+                    if  abs(unknown_color[0] - unknown_color[1]) < 5 and abs(unknown_color[0] - unknown_color[2]) > 20:
+                        shortest_dist = 0
+                        color_text = "yellow"
+                    elif shortest_dist > dist and list_colortexts[i] not in used_colors:
+                        shortest_dist = dist
+                        color_text = list_colortexts[i]
 
-            used_colors.append(color_text)
-            print(color_text)
+                used_colors.append(color_text)
+                print(color_text)
+                num_crewmates += 1
+
+        print("\nNumber of crewmantes: " + str(num_crewmates))
